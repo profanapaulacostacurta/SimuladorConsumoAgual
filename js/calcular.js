c75fe4188a1c24434448a5956a249d34ff42e09e
profanapaulacostacurta <123430271+profanapaulacostacurta@users.noreply.github.com>
2023-04-11 20:22:03 -0300

    Update calcular.js

--- js/calcular.js
+++ js/calcular.js
@@ -31,7 +31,7 @@ function calcularFaturaCategoriaA(consumoregistrado){
     const faixaA1=22.84, faixaA2=22.84, faixaA3=42.62, faixaA4=71.74, faixaA5=295.91;
     const adicionalA1=0, adicionalA2=3.95, adicionalA3=5.81, adicionalA4=7.46, adicionalA5=9.00;
         if (consumoregistrado <= faixa1){
-            faixa1 = consumoregistrado;
+            faixa1 = 0;
             isfaixa1 = 1;
             faixa2 = 0;
             faixa3 = 0
@@ -41,6 +41,7 @@ function calcularFaturaCategoriaA(consumoregistrado){
             if (consumoregistrado <= (faixa1+faixa2)){
                 faixa2 = consumoregistrado - faixa1;
                 isfaixa2 = 1;
+                faixa1 = 0
                 faixa3 = 0
                 faixa4 = 0;
                 faixa5 = 0;
@@ -48,15 +49,18 @@ function calcularFaturaCategoriaA(consumoregistrado){
                 if (consumoregistrado <= (faixa1+faixa2+faixa3)){
                     faixa3 = consumoregistrado - faixa1 - faixa2;
                     isfaixa3 = 1;
+                    faixa1 = 0;                                
                     faixa4 = 0;
                     faixa5 = 0;
                 } else {
                     if (consumoregistrado <= (faixa1+faixa2+faixa3+faixa4)){
                         faixa4 = consumoregistrado - faixa1 - faixa2 - faixa3;
                         isfaixa4 = 1;
+                        faixa1 = 0;
                         faixa5 = 0;
                     }else{
                         faixa5 = consumoregistrado-faixa1-faixa2-faixa3-faixa4;
+                        faixa1 = 0;
                         isfaixa5 = 1;
                     }
                 }
@@ -72,11 +76,12 @@ function calcularFaturaCategoriaB(consumoregistrado){
     const faixaB1=22.84, faixaB2=39.55; 
     const adicionalB1=0, adicionalB2=4.82; 
          if (consumoregistrado <= faixa1){
-            faixa1 = consumoregistrado;
+            faixa1 = 0;
             isfaxa1=1;
             faixa2 = 0;
         }else {
             faixa2 = consumoregistrado-faixa1;
+            faxa1 = 0;
         }    
     return (isfaixa1*faixaB1) + (isfaixa2*faixaB2) + (faixa1*adicionalB1) + (faixa2*adicionalB2);    
 }
@@ -87,11 +92,12 @@ function calcularFaturaCategoriaC(consumoregistrado){
     const faixaC1=54.05, faixaC2=54.05; 
     const adicionalC1=0, adicionalC2=6.14; 
     if (consumoregistrado <= faixa1){
-        faixa1 = consumoregistrado;
+        faixa1 = 0;
         isfaixa1=1;
         faixa2 = 0;
     }else {
         faixa2 = consumoregistrado-faixa1;
+        faixa1=0;
         isfaixa2=1;
     }    
     return (isfaixa1*faixaC1) + (isfaixa2*faixaC2) + (faixa1*adicionalC1) + (faixa2*adicionalC2);    
